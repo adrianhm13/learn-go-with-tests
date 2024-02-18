a08e79a7db42df8122fac023c6c147c5a08697de
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-02-18 21:34:26 +0100

    convert arrays into slices

--- array-slices/sum_test.go
+++ array-slices/sum_test.go
@@ -3,12 +3,14 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Sum collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	expected := 15
+		got := Sum(numbers)
+		expected := 15
 
-	if got != expected {
-		t.Errorf("got %d, expected %d given, %v", got, expected, numbers)
-	}
+		if got != expected {
+			t.Errorf("got %d, expected %d given, %v", got, expected, numbers)
+		}
+	})
 }
