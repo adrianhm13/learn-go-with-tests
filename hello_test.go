0124d6c78543d32a05997faab88fa001e7a4a9ab
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-02-18 11:02:02 +0100

    render World if string is empty

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Adrian")
-	want := "Hello, Adrian"
+	t.Run("Should say Hello with the name provided", func(t *testing.T) {
+		got := Hello("Adrian")
+		want := "Hello, Adrian"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Should say Hello, World if name is not provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
