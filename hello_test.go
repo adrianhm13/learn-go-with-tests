6f9c8cc9d4728c5dbab19d6ff73613194ed9ff4c
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-02-18 11:12:59 +0100

    execute Helper method in helper fn

--- hello_test.go
+++ hello_test.go
@@ -12,13 +12,16 @@ func TestHello(t *testing.T) {
 
 	t.Run("Should say Hello, World if name is not provided", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, World"
+		want := "Hello, aorld"
 
 		assertCorrectMessage(t, got, want)
 	})
 }
 
 func assertCorrectMessage(t testing.TB, got string, want string) {
+	// Method to let know the line where the test broke instead of the one from the helper function
+	t.Helper()
+
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
