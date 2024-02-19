03c6899df9693844e9906e20d021756d1d9b7e32
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-02-19 19:40:49 +0100

    adds function SumAllTails

--- array-slices/sum_test.go
+++ array-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Sum collection of 5 numbers", func(t *testing.T) {
@@ -12,5 +15,28 @@ func TestSum(t *testing.T) {
 		if got != expected {
 			t.Errorf("got %d, expected %d given, %v", got, expected, numbers)
 		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, expected []int) {
+		t.Helper()
+		if !slices.Equal(got, expected) {
+			t.Errorf("got %d, expected %d", got, expected)
+		}
+	}
+
+	t.Run("make the sum of multiple slices", func(t *testing.T) {
+		got := SumAllTails([]int{2, 5}, []int{0, 1, 2})
+		expected := []int{5, 3}
+
+		checkSums(t, got, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 5, 5})
+		expected := []int{0, 10}
+
+		checkSums(t, got, expected)
 	})
 }
