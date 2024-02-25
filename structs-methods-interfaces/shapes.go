e917e2aa0ce92da1676891b6cd4b7a8a4f091bcb
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-02-25 09:52:13 +0100

    adds triangle case

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -11,16 +11,25 @@ type Rectangle struct {
 	Height float64
 }
 
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
-}
-
 type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Height * t.Base) * 0.5
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -13,27 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %g, want %go", got, want)
-		}
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Height: 10.0, Base: 15.0}, hasArea: 75.0},
 	}
 
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
 
-		want := 72.0
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10.0}
-
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g, want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
