1d82f7b599843aeaddf1f5b1cd445147426f323c
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-03-03 17:45:07 +0100

    feat(maps): adds not word found use case

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,14 +1,32 @@
 package maps
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is a test"}
 
-	got := dictionary.Search("test")
-	want := "this is a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("word not found", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		assertError(t, err, ErrNotFound)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
