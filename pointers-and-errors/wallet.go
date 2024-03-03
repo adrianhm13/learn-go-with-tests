05b22825ce18129e5845e33106066a19b24e86ad
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-03-03 10:02:52 +0100

    refactor(pointers and errors)

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -1,6 +1,9 @@
 package wallet
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Stringer interface {
 	String() string
@@ -12,12 +15,20 @@ type Wallet struct {
 	balance Bitcoin
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
 	w.balance -= amount
+
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
